88fb1747f3ae2a81d4579394c3b0027d67e771c3
chenhaowei1314 <602787960@qq.com>
2022-09-17 20:55:31 +0800

    第一次提交

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author chenhaowei
+ * @date 2022/9/17 - 20:48
+ * @description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
